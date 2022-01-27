d28bb7c79694e80dc5f164bbc7717da235f66470
SosiskaPish <aziz.10.03@mail.ru>
2022-01-27 13:20:54 +0300

    clean start template, add normalize.css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse rem nisi illo quisquam labore, similique fuga
+        commodi, temporibus tempora animi nulla iste. Porro deleniti nostrum voluptas ipsam laudantium enim vel!
+      </h1>
+    </div>
+  )
+}
+
+export default App
