4c6e574fec2e011619cd806f783fcdbbca88a541
SosiskaPish <aziz.10.03@mail.ru>
2022-02-11 21:01:47 +0300

    add card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
-import { Card, CardContent, Container, Typography } from '@mui/material'
+import { Card, CardContent, Container, Typography, List, ListItem } from '@mui/material'
 
 function Cards(props) {
   return (
-    <Container sx={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between'}}>
+    <Container sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
       {props.allEpisode.map(episode => (
-        <Card key={episode.episode_id} sx={{ maxWidth: 275, margin: 2}}>
+        <Card key={episode.episode_id} sx={{ maxWidth: 275, margin: 2 }}>
           <CardContent>
-            <Typography gutterBottom>{episode.title}</Typography>
+            <Typography variant="h4" gutterBottom>
+              {episode.title}
+            </Typography>
+            <Typography variant="h6" gutterBottom>
+              Air date: {episode.air_date}{' '}
+            </Typography>
+            <Typography variant="button" gutterBottom>
+              Season: {episode.season}
+            </Typography>
           </CardContent>
+          <List component="nav" aria-label="mailbox folders">
+            {episode.characters.map(character => (
+              <ListItem key={character} button>
+                {character}
+              </ListItem>
+            ))}
+          </List>
         </Card>
       ))}
     </Container>
