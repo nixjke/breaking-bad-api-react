74d2186102b1ca0707b2c98ed632fafad562b026
SosiskaPish <aziz.10.03@mail.ru>
2022-03-17 11:51:56 +0300

    fix style for Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,19 +3,22 @@ import { Card, CardContent, Container, Typography, List, ListItem } from '@mui/m
 
 function Cards(props) {
   return (
-    <Container sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+    <Container sx={{ display: 'flex', flexWrap: 'wrap' }}>
       {props.allEpisode.map(episode => (
-        <Card key={episode.episode_id} sx={{ maxWidth: 275, margin: 2 }}>
+        <Card key={episode.episode_id} sx={{ maxWidth: 275, margin: 2, bgcolor: '#34495E', color: 'white' }}>
           <CardContent>
             <Typography variant="h4" gutterBottom>
               {episode.title}
             </Typography>
+            <hr />
             <Typography variant="h6" gutterBottom>
               Air date: {episode.air_date}{' '}
             </Typography>
+            <hr />
             <Typography variant="button" gutterBottom>
               Season: {episode.season}
             </Typography>
+            <hr />
           </CardContent>
           <List component="nav" aria-label="mailbox folders">
             {episode.characters.map(character => (
